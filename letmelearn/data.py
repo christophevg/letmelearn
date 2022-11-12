66122d971fa62ac0179a9bc68a530cecc40816c6
Christophe VG <contact@christophe.vg>
2022-11-12 15:11:24 +0100

    reduce to baseweb only

--- letmelearn/data.py
+++
@@ -1,7 +0,0 @@
-import os
-from pymongo import MongoClient
-
-DB       = os.environ.get("MONGODB_URI", "mongodb://localhost:27017/letmelearn")
-mongo    = MongoClient(DB)
-database = DB.split("/")[-1].split("?")[0]
-db       = mongo[database]
