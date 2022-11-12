9d96a5dc6d1b54befd143a7235c8f2e5d5fe975c
Christophe VG <contact@christophe.vg>
2022-11-12 15:54:03 +0100

    removing late server config updates

--- letmelearn/__init__.py
+++ letmelearn/__init__.py
@@ -4,13 +4,8 @@ __version__ = "0.0.4"
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv(usecwd=True))
 
-import os
-
 import letmelearn.setup
 
 from baseweb.web import server
-
-server.config["TEMPLATES_AUTO_RELOAD"] = True
-server.config["SECRET_KEY"] = os.environ.get("APP_SECRET_KEY", default="local")
 
 import letmelearn.pages.index
