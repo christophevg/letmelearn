3c747345d2f6e7c2ff9f2ef18d1d67468acfe010
Christophe VG <contact@christophe.vg>
2022-11-12 18:15:03 +0100

    trying with explicit monkey patching for ssl

--- letmelearn/__init__.py
+++ letmelearn/__init__.py
@@ -1,5 +1,8 @@
 __version__ = "0.0.4"
 
+import eventlet
+eventlet.monkey_patch()
+
 # load the environment variables for this setup
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv(usecwd=True))
