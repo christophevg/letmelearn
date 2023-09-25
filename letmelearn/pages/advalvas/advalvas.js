15bd9e7147f0aea2d725fc437d7c5b4de19a946f
Christophe VG <contact@christophe.vg>
2023-09-25 19:25:09 +0200

    added to and improved ad valvas news

--- letmelearn/pages/advalvas/advalvas.js
+++ letmelearn/pages/advalvas/advalvas.js
@@ -1,37 +1,80 @@
 var Home = {
   template : `
 <ProtectedPage>
-  <h1>🎓 Ad Valvas...</h1>
+  <h1 align="center">🎓 Ad Valvas...</h1>
+
+	<br>
+
+	<h2 align="center">📣 Nieuwigheden</h2>
+
+	<br>
 
   <v-layout>
     <v-flex xs12 sm6 offset-sm3>
-      <v-card>
-        <v-card-title primary-title>
-          <div>
-            <h3 class="headline mb-0">📣 Nieuwigheden</h3>
 
-            <br>
+			<v-card>
+        <v-layout row>
+          <v-flex xs7>
+            <v-card-title primary-title>
+              <div>
+                <div class="headline">Meerdere topics per quiz</div>
+								<div class="grey--text">25 september 2023</div>
 
-            <h4>Meerdere goede antwoorden</h4>
+            		<p>
   
-            <p>
-  
-              Je kan vanaf nu meerdere goede antwoorden voorzien door deze te
-              scheiden door middel van een verticale streep, een zogenaamd
-              "pipe"-symbool: "|".
-  
-              <br><br>
-  
-              <v-img
-                src="/app/static/images/news/multiple-possible-answers.png"
-                max-width="400"
-              ></v-img>
+									Je kan vanaf nu meerdere topics selecteren om een quiz te doen.
 
-            </p>
+								</p>
 
-          </div>
-        </v-card-title>
+              </div>
+            </v-card-title>
+          </v-flex>
+          <v-flex xs5>
+						<div style="padding:15px">
+	            <v-img
+	              src="/app/static/images/news/multiple-topics.png"
+	              height="125px"
+	              contain
+	            ></v-img>
+						</div>
+          </v-flex>
+        </v-layout>
       </v-card>
+			
+			<br>
+			
+			<v-card>
+        <v-layout row>
+          <v-flex xs7>
+            <v-card-title primary-title>
+              <div>
+                <div class="headline">Meerdere goede antwoorden</div>
+								<div class="grey--text">25 september 2023</div>
+
+		            <p>
+  
+		              Je kan vanaf nu meerdere goede antwoorden voorzien door deze te
+		              scheiden door middel van een verticale streep, een zogenaamd
+		              "pipe"-symbool: "|".
+
+								</p>
+
+              </div>
+            </v-card-title>
+          </v-flex>
+          <v-flex xs5>
+						<div style="padding:15px">
+	            <v-img
+	              src="/app/static/images/news/multiple-possible-answers.png"
+	              height="125px"
+	              contain
+	            ></v-img>
+						</div>
+          </v-flex>
+        </v-layout>
+      </v-card>
+
+
     </v-flex>
   </v-layout>
 
