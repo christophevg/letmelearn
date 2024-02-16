05102807986ac635ee66fbb7942584c6e3fcf6ef
Christophe VG <contact@christophe.vg>
2024-02-16 11:18:40 +0100

    Update BasicQuestion.js

--- letmelearn/components/questions/BasicQuestion.js
+++ letmelearn/components/questions/BasicQuestion.js
@@ -54,7 +54,7 @@ Vue.component("BasicQuestionAskingWritten", {
         <v-card-title primary-title class="justify-center">
           <h3 class="headline mb-0">{{ format(question) }}</h3><br>
           <div style="width:100%; text-align:center;margin-top:20px;">
-            <v-text-field autofocus v-model="written" :error="outcome === false" :background-color="outcome ? 'success' : 'error'"></v-text-field>
+            <v-text-field readonly autofocus v-model="written" :error="outcome === false" :background-color="outcome ? 'success' : 'error'"></v-text-field>
             <h1 v-if="outcome === false" style="color:green">{{ format(expected) }}</h1>
             <h1 v-if="outcome === false">
               <template v-for="possible_answer in expected">
